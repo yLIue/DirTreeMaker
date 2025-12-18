570da5da411757ea431e9efbc8d6e880b1936c73
yLIue <2138347243@qq.com>
2025-12-18 11:23:57 +0800

    对上述开发的内容进行应用

--- DirTreeMaker/MainWindow.xaml.cs
+++ DirTreeMaker/MainWindow.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System.Text;
+﻿using DirTreeMaker.Logic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Security.Policy;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -14,11 +19,64 @@ namespace DirTreeMaker
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        private string _outputText;
+        private string _inputText;
+        private readonly TreeGenerator logic;
         public MainWindow()
         {
+            logic = new TreeGenerator();
             InitializeComponent();
+            this.DataContext = this;
         }
+
+        public void GenerateTree()
+        {
+            OutputText = logic.GenerateTree(InputText);
+        }
+
+        private void GitHubClick(object sender, RoutedEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = "https://github.com/yLIue/DirTreeMaker",
+                UseShellExecute = true
+            });
+        }
+
+        public string OutputText
+        {
+            get => _outputText;
+            set
+            {
+                if (_outputText != value)
+                {
+                    _outputText = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string InputText
+        {
+            get => _inputText;
+            set
+            {
+                if (_inputText != value)
+                {
+                    _inputText = value;
+                    OnPropertyChanged();
+                    GenerateTree();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler ?PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
     }
-}
+} 
